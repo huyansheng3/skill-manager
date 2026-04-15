07845bad2e12aeb49f3f81610c19b39e192e947d
huyansheng <huyansheng@kuaishou.com>
2026-04-15 21:42:46 +0800

    Fix: Three project structure issues
    1. Add placeholder structs for SkillListViewModel and SkillManagerMainView to allow compilation
    2. Remove unnecessary NSApplicationDelegateAdaptorInfo from Info.plist (not needed for SwiftUI @main)
    3. Move SkillManagerApp.swift file reference from root group to App group in Xcode project

--- SkillManager/App/SkillManagerApp.swift
+++ SkillManager/App/SkillManagerApp.swift
@@ -13,3 +13,15 @@ struct SkillManagerApp: App {
         .windowResizability(.contentSize)
     }
 }
+
+// Placeholder - will be implemented in later tasks
+class SkillListViewModel: ObservableObject {
+    public init() {}
+}
+
+// Placeholder - will be implemented in later tasks
+struct SkillManagerMainView: View {
+    var body: some View {
+        Text("Skill Manager")
+    }
+}
